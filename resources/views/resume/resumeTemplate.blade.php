12e3e054bae6902196fc7a17334c7e724c65f617
faizalyaacob <jalpejal74123@gmail.com>
2021-12-05 12:58:55 +0800

    Added resume template

---
+++ resources/views/resume/resumeTemplate.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+    <title>KUPTM Template Resume</title>
+</head>
+<body class="bg-light">
+    <div class="container p-5">
+        <div class="row">
+            <div class="col-lg-4 bg-dark text-white text-center py-4">
+                <div class="header-left">
+                    <img src="" alt="" class="img-thumbnail rounded-circle mb-2">
+                    <h6 class="display-5">Student Name</h6>
+                    <h4 class="lead text-uppercase text-white-50 mb-4">Position</h4>
+                </div>
+
+                <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pill">Contact</h5>
+                    <ul class="list-unstyled text-white-35 ml-2 py-2 text-left">
+                        <li class="list-item">
+                            <i class="fas fa-mobile-alt mx-4"></i>
+                            Phone Number
+                        </li>
+                        <li class="list-item">
+                            <i class="fas fa-envelope-open-text mx-4"></i>
+                            Email Address
+                        </li>
+                        <li class="list-item">
+                            <i class="fab fa-linkedin-in mx-4"></i>
+                            Linked in URL
+                        </li>
+                        <li class="list-item">
+                            <i class="fas fa-map-marker-alt mx-4"></i>
+                            Street Name, City
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pill">Skills</h5>
+                    <ul class="list text-white-35 ml-2 py-2 text-left text-capitalize">
+                        <li class="list-item">programming skills</li>
+                        <li class="list-item">communication skills</li>
+                        <li class="list-item">presentation skills</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h5 class="text-uppercase bg-white text-dark py-2 rounded-pill">Education</h5>
+                    <ul class="list text-white-35 ml-2 py-2 text-left text-capitalize">
+                        <li class="list-item">Education 1</li>
+                        <li class="list-item">Education 2</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-8 bg-light text-dark py-4 px-5">
+                <div class="header-right">
+                    <h4 class="text-uppercase">Profile</h4>
+                    <hr>
+                    <p>
+                        Description about yourself
+                    </p>
+                </div>
+                <div>
+                    <h4 class="text-uppercase">Work Experience</h4>
+                    <hr>
+                    <ul class="list">
+                        <li class="list-item">
+                            <h5 class="display-6 text-uppercase">Last Position </h5>
+                            <h6 class="text-uppercase text-black-50">Company Name / Start month and year - End month and year</h6>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem Ipsum is simply dummy text of the printing and typesetting industry. ... 
+                                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+                            </p>
+                        </li>
+                        <li class="list-item">
+                            <h5 class="display-6 text-uppercase">AI Engineer </h5>
+                            <h6 class="text-uppercase text-black-50">Nexent Sdn Bhd / January 2021 - Present</h6>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna. Lorem Ipsum is simply dummy text of the printing and typesetting industry. ... 
+                                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    
+</body>
+</html>
