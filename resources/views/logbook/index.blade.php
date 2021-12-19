a9a9e120f4cd2b75847b9a548161dd7245d8849b
faizalyaacob <jalpejal74123@gmail.com>
2021-12-19 12:04:57 +0800

    Added logbook frontend Design

--- resources/views/logbook/index.blade.php
+++ resources/views/logbook/index.blade.php
@@ -287,35 +287,70 @@
     {{-- ain buat --}}
     <div class="row">
 
-        <div class="ml-5 mt-3">
-            <div class="row">
-                <h5>Generate new week</h5>
-            </div>
-            <div class="row mt-2">
-                <form action="POST">
-                    @csrf 
-                    <input class="form-control" type="text" id="date_monday" placeholder="Select date for new week"/>
-                </form>
-            </div>   
-        </div>
-
-        <div class="col-lg-8 mt-5 mx-auto">
+        <div class="col-lg-12 mt-5 mx-auto">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title">LOGBOOK </h4>
+                    <h4 class="header-title">Student's Logbook (By Week)</h4>
                     <div id="log" class="according accordion-s2 gradiant-bg">
 
                         @for ($i = 1; $i <= 14; $i++)
                             <div class="card">
                                 <div class="card-header">
-                                    <a class="collapsed card-link" data-toggle="collapse" href="#w{{$i}}">Week {{$i}}</a>
+                                    <a class="collapsed card-link" data-toggle="collapse" href="#w{{$i}}">Week {{$i}} </a>
+                                    <span class="badge badge-light"></span>
                                 </div>
                                 <div id="w{{$i}}" class="collapse" data-parent="#log">
                                     <div class="card-body">
                                         {{-- if else database empty --}}
-                                        <div class="text-center">
-                                            <p>There is no data.</p>
-                                            <a href="#" type="button" class="btn btn-success btn-md mb-3 mt-3"><span class="ti-plus"></span> Add Details</a>
+                                        <div>
+                                            <h3 class="text-center"><span class="badge badge-pill badge-light">Status: Not Validate</span></h3>
+                                            <form action="POST">
+                                                @csrf 
+                                                <div class="form-group col-lg-3 text-center mx-auto">
+                                                    <label for="date-{{ $i }}">Select Date</label>
+                                                    <input class="form-control text-center" type="text" id="date-week-{{$i}}" placeholder="Example : 19/2/2021" required/>
+                                                </div>
+                                                
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Monday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Tuesday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Wednesday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Thursday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Friday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Saturday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <label for="date-{{ $i }}">Sunday</label>
+                                                    <textarea class="form-control" id="text-{{ $i }}" maxlength="350" cols="20" rows="2" placeholder="Not more than 350 characters"></textarea>
+                                                </div>
+                                                
+                                                <div class="form-group-inline ">
+                                                    <input class="btn btn-primary btn-sm pull-right mb-3" type="submit" value="Save" id="submit-{{ $i }}" />
+                                                    <button class="btn btn-secondary btn-sm pull-right mb-3 mr-3">Request for Validation</button>
+                                                </div>                                              
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
@@ -337,25 +372,24 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
     <script> 
-        $(document).ready(function() {
-            $('#date_monday').datepicker({
-                beforeShowDay: enableMonday,
-                dateFormat: 'dd/mm/yy'
-            });
-    
-            function enableMonday(date){
-                var day = date.getDay(); 
-                return [day==1]
-            };
 
-            // function autoGenerateWeek(){
-            //     var date2 = $('#date_monday').datepicker({beforeShowDay: enableMonday,dateFormat: 'dd/mm/yy'});
-            //     for (let i = 0; i < 5; i++) { 
-            //         console.log(date2.getDate()+(i+1))
-            //     }
-            // }
-            // autoGenerateWeek();
-        }); 
+        function enableMonday(date){
+            var day = date.getDay(); 
+            return [day==1]
+        };
+
+        for (var i=0; i<14; i++){
+            let week_text = i.toString()
+            $(document).ready(function() {
+                $('#date-week-'+week_text).datepicker({
+                    beforeShowDay: enableMonday,
+                    dateFormat: 'dd/mm/yy'
+                });
+            });
+
+        }
+ 
+
     </script>
     
 @endsection
