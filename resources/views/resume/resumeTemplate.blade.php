721e028ba0b9985e0c100f1f9e66d8a058fab104
nurain <nurain936@gmail.com>
2022-01-03 10:53:20 +0800

    css js 3

--- resources/views/resume/resumeTemplate.blade.php
+++ resources/views/resume/resumeTemplate.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/dw/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/dw/all.min.css') }}">
     <title>KUPTM Template Resume</title>
 </head>
 <body class="bg-light">
