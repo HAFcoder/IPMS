0428d87e8700f1dfa3aedfd8ffc696276d9dc0d2
hamzah014 <43804443+hamzah014@users.noreply.github.com>
2022-04-20 13:12:19 +0800

    update decline mail

--- resources/views/emails/declineMail.blade.php
+++ resources/views/emails/declineMail.blade.php
@@ -2,7 +2,7 @@
 # {{ $details['title'] }}
   
 Mr./Ms., <br>
-Thank you so much for extending this opportunity to our student {{ $details['name'] }}. {{ $details['company'] }} is a wonderful organization and
+Thank you so much for extending this opportunity to our student {{ $details['name'] }} ({{ $details['id'] }} ). {{ $details['company'] }} is a wonderful organization and
 Though it was a difficult decision, {{ $details['name'] }} decided to accept another internship that focuses more directly on the skill set that he/she is looking to grow. We appreciate the offer to work with your team.
 
 <br>
